83f1719074ba2fabc01ef4c2505d84a801eba48e
iamguystephane <gstephane138@gmail.com>
2025-03-31 00:06:31 +0100

    installed nodemon for automatic server restart when I make changes

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const sayHi = require('./hello-function') //invoking the mind-granade module, even though we didn't export it. Importing the file automatically runs the code in this file.
-const names = require('./names')
-require('./mind-granade') // this will run the code in the mind-grenade file, even though we didn't export it. Importing the file automatically runs the code in this file.
-sayHi(names.john);
+const http = require("http");
 
-sayHi(names.stephane);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Hello, and welcome to our home page!");
+    return;
+  }
+  if (req.url === "/about") {
+    res.end("Here is our short history!");
+    return;
+  }
+  if (req.url === "/contact") {
+    res.end("Here is our contact page!");
+    return;
+  }
+  res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you are looking for</p>
+        <a href="/">back home</a>
+    `);
+});
+
+server.listen(5000, () => {
+  console.log("Server is listening on http://localhost:5000");
+});
